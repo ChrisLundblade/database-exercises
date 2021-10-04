69aa4a75ba51b90407fd9c8a35b5abef6a5b8571
Christopher Lundblade <chris.lundblade@gmail.com>
2021-10-04 10:24:46 -0500

    I forgot a few entries in albums_seeder.sql

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -6,7 +6,7 @@ VALUES ('Michael Jackson', 'Thriller', 1982,'Pop, post-disco, funk, rock', 48.9)
        ('AC/DC', 'Back In Black', 1980,'Hard rock',29.6),
        ('Witney Houston', 'The Bodyguard', 1992,'R&B, soul, pop, soundtrack',32.4),
        ('Meat Loaf', 'Bat Out of Hell', 1977, 'Hard rock, glam rock, progressive rock', 21.7),
-       ('Eagles', 'Their Greatest Hits 1971-1975',1976,'Country rock, soft rock, folk rock',21.7),
+       ('Eagles', 'Their Greatest Hits (1971-1975)',1976,'Country rock, soft rock, folk rock',21.7),
        ('Pink Floyd','The Dark Side of the Moon', 1973, 'Progressive rock', 24.4),
        ('Eagles','Hotel California',1976,'Soft rock',31.5),
        ('Bee Gees','Saturday Night Fever',1977,'Disco',21.6),
@@ -15,10 +15,13 @@ VALUES ('Michael Jackson', 'Thriller', 1982,'Pop, post-disco, funk, rock', 48.9)
        ('Various artists','Grease: The Original Soundtrack from the Motion Picture',1978, 'Rock and roll',14.4),
        ('Led Zeppelin','Led Zeppelin IV',1971,'Hard rock, heavy metal, folk rock',29.0),
        ('Michael Jackson','Bad',1987,'Pop, rhythm and blues, funk and rock',20.7),
-       ('Alanis Morissette','Jagged Little Pill',1995,'Alternative rock',
-24.3),
+       ('Alanis Morissette','Jagged Little Pill',1995,'Alternative rock',24.3),
        ('Michael Jackson','Dangerous',1991,'New jack swing, R&B and pop',17.0),
        ('Celine Dion','Falling into You',1996,'Pop, soft rock',21.1),
+        ('The Beatles', 'Sgt. Pepper\'s Lonely Hearts Club Band', 1967, 'Rock', 18.2),
+       ('Various artists', 'Dirty Dancing', 1987, 'Pop, rock R&B', 17.9),
+        ('Adele', '21', 2011, 'Pop, soul', 26.4),
+        ('Celine Dion', 'Let\'s Talk About Love', 1997, 'Pop, soft rock', 19.3),
        ('Metallica','Metallica',1991,'Heavy metal',25.2),
        ('The Beatles','1', 	2000, 'Rock', 23.5),
        ('The Beatles', 'Abbey Road', 1969, 'Rock', 14.4),
@@ -31,5 +34,4 @@ VALUES ('Michael Jackson', 'Thriller', 1982,'Pop, post-disco, funk, rock', 48.9)
        ('Nirvana', 'Nevermind',	1991, 'Grunge, alternative rock', 16.7),
        ('Santana', 'Supernatural', 1999, 'Latin rock', 20.5),
        ('Guns N\' Roses', 'Appetite for Destruction', 1987, 'Hard rock', 21.9),
-       ('Elton John', 'Goodbye Yellow Brick Road', 1973, 'rock, pop rock, glam rock', 8.5)
-
+       ('Elton John', 'Goodbye Yellow Brick Road', 1973, 'rock, pop rock, glam rock', 8.5);
---
+++ quotes_seeder.sql
@@ -0,0 +1,4 @@
+USE codeup_test_db;
+
+INSERT INTO quotes (author_first_name, author_last_name, content)
+VALUES ();
