a7994b0e0e0aaa836edb191553027ba57640a99f
Christopher Lundblade <chris.lundblade@gmail.com>
2021-10-05 10:27:30 -0500

    completed order_by_exercises.sql

---
+++ order_by_exercises.sql
@@ -0,0 +1,27 @@
+USE employees;
+
+SHOW TABLES;
+
+DESCRIBE employees;
+
+#This is one I actually changed for this assignment
+SELECT * FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+    ORDER BY last_name DESC, first_name DESC;
+
+SELECT * FROM employees
+WHERE last_name LIKE ('E%')
+   OR last_name LIKE '%E';
+
+#And this one too
+SELECT * FROM employees
+WHERE last_name LIKE ('E%')
+  AND last_name LIKE '%E'
+    ORDER BY emp_no DESC;
+
+SELECT * FROM employees
+WHERE last_name LIKE ('%q%');
+
+SELECT * FROM employees
+WHERE last_name LIKE ('%q%')
+  AND last_name NOT LIKE ('%qu%');
