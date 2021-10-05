4178be2625cf40c5cd4c7a079ccb087c2f11c591
Christopher Lundblade <chris.lundblade@gmail.com>
2021-10-05 11:04:53 -0500

    revised order_by_exercises.sql and completed limit_exercises.sql

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -6,17 +6,32 @@ DESCRIBE employees;
 
 #This is one I actually changed for this assignment
 SELECT * FROM employees
+    WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+    ORDER BY  first_name;
+#and this
+SELECT * FROM employees
+    WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+    ORDER BY first_name, last_name;
+#and this
+SELECT * FROM employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya')
-    ORDER BY last_name DESC, first_name DESC;
+ORDER BY last_name, first_name;
+#and this
+SELECT * FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY last_name DESC, first_name DESC;
 
 SELECT * FROM employees
 WHERE last_name LIKE ('E%')
    OR last_name LIKE '%E';
 
-#And this one too
+
 SELECT * FROM employees
 WHERE last_name LIKE ('E%')
-  AND last_name LIKE '%E'
+  AND last_name LIKE '%E';
+#And this... i think. Instructions were confusing and numerous
+SELECT * FROM employees
+    WHERE last_name LIKE ('%E%')
     ORDER BY emp_no DESC;
 
 SELECT * FROM employees
