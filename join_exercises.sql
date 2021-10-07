da095d64aba609e246b52d06eef43efe88a4b292
Christopher Lundblade <chris.lundblade@gmail.com>
2021-10-07 17:02:22 -0500

    did some more bonuses in join_exercises.sql

--- join_exercises.sql
+++ join_exercises.sql
@@ -61,3 +61,38 @@ JOIN employees e2 ON e2.emp_no = dm.emp_no
 WHERE de.to_date > NOW()
 AND dm.to_date > NOW()
 ORDER BY d.dept_name, e.emp_no;
+
+#What is the name of everyone who has worked or is working as an ‘Assistant Engineer’?
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS Name, t.title AS Name
+FROM employees e
+JOIN titles t
+ON e.emp_no = t.emp_no
+WHERE t.title = 'Assistant Engineer';
+
+# What are the DOBs of all current senior engineers?
+SELECT e.birth_date AS 'Date of Birth', CONCAT(e.first_name, ' ', e.last_name), t.title FROM employees e
+JOIN titles t ON e.emp_no = t.emp_no
+WHERE t.to_date > NOW() AND
+      t.title = 'Senior Engineer'
+ORDER BY e.emp_no;
+
+# What is the most common title in the ‘Marketing’ department?
+#This is not finished
+SELECT t.title, COUNT(t.emp_no) AS 'Number of Employees'
+FROM titles t
+Where t.to_date > NOW()
+
+GROUP BY t.title
+ORDER BY COUNT(t.emp_no) DESC;
+
+# What is the most common birthday in the company? least common?
+
+# How many male and female employees currently work in the ‘Finance’ department?
+
+# What is the average current salary for each department?
+
+# What was the average salary in the 80s (a salary that started and ended in the 80s)? By department?
+
+# What are the names of all ‘Staff’ employees in the ‘Research’ department?
+
+# What is name of the lowest paid ‘Staff’ members in the ‘Research’ department (make sure the salary and employment in the department is current).
